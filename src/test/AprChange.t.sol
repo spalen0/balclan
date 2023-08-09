e44b046f48709ad1630aa66cc617e31df5e6f147
spalen0 <spalen@proton.me>
2023-08-09 23:28:11 +0200

    test: additional apr test

--- src/test/AprChange.t.sol
+++ src/test/AprChange.t.sol
@@ -9,32 +9,32 @@ contract AprChangeTest is Setup {
         super.setUp();
     }
 
-    // function test_checkAaveAprChange(uint256 _amount, uint16 _percentChange) public {
-    //     vm.assume(_amount > minFuzzAmount && _amount < maxFuzzAmount);
-    //     _percentChange = uint16(bound(uint256(_percentChange), 10, MAX_BPS));
+    function test_checkAaveAprChange(uint256 _amount, uint16 _percentChange) public {
+        vm.assume(_amount > minFuzzAmount && _amount < maxFuzzAmount);
+        _percentChange = uint16(bound(uint256(_percentChange), 10, MAX_BPS));
 
-    //     // TODO: adjust the number to base _perfenctChange off of.
-    //     uint256 _delta = (_amount * _percentChange) / MAX_BPS;
+        // TODO: adjust the number to base _perfenctChange off of.
+        uint256 _delta = (_amount * _percentChange) / MAX_BPS;
 
-    //     (uint256 currentAprSupply, uint256 currentApyBorrow) = strategy.aaveRates(0);
-    //     console.log("currentAprSupply: %s", currentAprSupply);
-    //     console.log("currentApyBorrow: %s", currentApyBorrow);
+        (uint256 currentAprSupply, uint256 currentApyBorrow) = strategy.aaveRates(0);
+        console.log("currentAprSupply: %s", currentAprSupply);
+        console.log("currentApyBorrow: %s", currentApyBorrow);
 
-    //     // Should be greater than 0 but likely less than 100%
-    //     assertGt(currentAprSupply, 0, "ZERO");
-    //     assertLt(currentAprSupply, 1e18, "+100%");
-    //     assertGt(currentApyBorrow, 0, "ZERO");
-    //     assertLt(currentApyBorrow, 1e18, "+100%");
+        // Should be greater than 0 but likely less than 100%
+        assertGt(currentAprSupply, 0, "ZERO");
+        assertLt(currentAprSupply, 1e18, "+100%");
+        assertGt(currentApyBorrow, 0, "ZERO");
+        assertLt(currentApyBorrow, 1e18, "+100%");
 
-    //     (uint256 negativeAprSupply, uint256 negativeApyBorrow) = strategy.aaveRates(-int256(_delta));
-    //     // The apr should go up if deposits go down
-    //     assertLt(currentAprSupply, negativeAprSupply, "negative supply change");
-    //     assertLt(currentApyBorrow, negativeApyBorrow, "negative borrow change");
+        (uint256 negativeAprSupply, uint256 negativeApyBorrow) = strategy.aaveRates(-int256(_delta));
+        // The apr should go up if deposits go down
+        assertLt(currentAprSupply, negativeAprSupply, "negative supply change");
+        assertLt(currentApyBorrow, negativeApyBorrow, "negative borrow change");
 
-    //     (uint256 possitveAprSupply, uint256 positiveApyBorrow) = strategy.aaveRates(int256(_delta));
-    //     assertGt(currentAprSupply, possitveAprSupply, "positive supply change");
-    //     assertGt(currentApyBorrow, positiveApyBorrow, "positive borrow change");
-    // }
+        (uint256 possitveAprSupply, uint256 positiveApyBorrow) = strategy.aaveRates(int256(_delta));
+        assertGt(currentAprSupply, possitveAprSupply, "positive supply change");
+        assertGt(currentApyBorrow, positiveApyBorrow, "positive borrow change");
+    }
 
     function test_checkCompoundAprChange(
         uint256 _amount,
